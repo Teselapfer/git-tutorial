b9c5fa5dd586868cfc0b2e4523234978168859ad
Teselapfer <schwarz44571@gmail.com>
2023-11-30 14:21:16 +0900

    add: Add rsp.rb

---
+++ rsp.rb
@@ -0,0 +1,18 @@
+
+loop do   
+    puts 'choose your hand. 1.rock 2.scissors 3.paper'
+    ophand = gets.to_i
+    enhand = rand(1..3)
+    
+    puts "your hand is #{hands(ophand)}."
+    puts "enemy hand is #{hands(enhand)}."
+    
+    puts case   
+            when ophand == enhand - 1 || ophand == enhand + 2 then 'you win.'
+            when ophand == enhand + 1 || ophand == enhand - 2  then 'you lose.'
+            when ophand == enhand  then 'draw.'
+         end
+    
+    puts 'finish? 1.yes'
+    break if gets.to_i == 1
+end
